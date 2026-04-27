98b18602faaa75938855c137f74efc5ef4653290
Luchox <luisetiem.munoz@alumnos.ulagos.cl>
2026-04-26 20:48:09 -0400

    SUBIENDO ARCHIVOS

--- fresh-project_Tele_Alae/routes/index.tsx
+++ fresh-project_Tele_Alae/routes/index.tsx
@@ -1,25 +1,23 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
-  return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+  return(
+    <div class='min-h-screen bg-indigo-900 flex items-center justify-center'>
+      <div class='bg-indigo-500/40 border border-white/40 rounded-3xl p-10 w-[500px]'>
+        <h1 class='text-4xl text-center text-white font-bold mb-2'>
+          Tele Alae
+        </h1>
+        <p class='text-center text-white/70 mb-6'>
+        Sistema de Telemedicina Chiloé
         </p>
-        <Counter count={count} />
+        <div class="flex mb-6">
+          <button class="flex-1 text-white border-b-2 border-white py-2">
+            Inicio de sesión
+          </button>
+          <button class="flex-1 text-white/60 border-b-2 border-transparent py-2">
+            Crear Cuenta
+          </button>
+        </div>
+
       </div>
     </div>
-  );
+  )
 }
