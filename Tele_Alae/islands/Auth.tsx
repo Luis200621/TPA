fb285b992fb303d704a3b4fef141be26e5863faf
Luchox <luisetiem.munoz@alumnos.ulagos.cl>
2026-05-04 14:50:05 -0400

    añadiendo comentarios para ordenar codigo

--- Tele_Alae/islands/Auth.tsx
+++ Tele_Alae/islands/Auth.tsx
@@ -6,24 +6,25 @@ export default function Auth() {
   return (
     <div class="min-h-screen bg-[#211C84] flex flex-col items-center justify-center">
 
-      {/*TÍTULO */}
+      {/*Logo */}
       <img 
       src="/Logo_a_color.png" 
       class="absolute top-6 left-6 w-44 h-44 object-contain"
       />
+      {/*Titulo*/}
       <h1 class="text-7xl text-white font-bold mb-2">
         Tele Alae
       </h1>
-      
+      {/*Sub-titulo*/}
       <div class="p-0.5 mb-12" >
       <p class="text-white/70 mb-2">
         Sistema de Telemedicina Chiloe
       </p>
       </div>
-      {/* 🔹 CUADRO */}
+      {/* Cuadro */}
       <div class="bg-[#7473D1] border border-white/30 rounded-3xl p-10 w-full max-w-[1000px] shadow-2xl backdrop-blur-lg">
 
-        {/* Tabs */}
+        {/* Cambio de inicio de sesion y crear cuenta*/}
         <div class="flex mb-6">
           <button 
             onClick={() => setTab("login")}
@@ -44,7 +45,7 @@ export default function Auth() {
           </button>
         </div>
 
-        {/* LOGIN */}
+        {/*Cuadros de texto del login*/}
         {tab === "login" && (
           <form class="flex flex-col gap-4">
             <div>
@@ -54,8 +55,9 @@ export default function Auth() {
                 placeholder="correo@algo.com"
                 class="w-full mt-1 p-3 rounded-full bg-[#4D55CC] text-white outline-none placeholder-white/60"
               />
+              {/*boton que crea un pop up el cual sirve parab enviar contraseña temporal*/}
               <button class="text-white">
-              E̲n̲v̲i̲a̲r̲ C̲o̲r̲r̲e̲o̲
+              E̲n̲v̲i̲a̲r̲ C̲o̲n̲t̲r̲a̲s̲e̲ñ̲a̲
               </button>
             </div>
 
@@ -67,14 +69,14 @@ export default function Auth() {
                 class="w-full mt-1 p-3 rounded-full bg-[#4D55CC] text-white outline-none placeholder-white/60"
               />
             </div>
-
-            <button class="mt-4 bg-indigo-800 hover:bg-[#4D55CC] text-white py-3 rounded-full w-40 mx-auto">
+            {/*Boton de inicio de sesion*/}
+            <button class="mt-4 bg-indigo-800 hover:bg-[#B5A8D5] text-white py-3 rounded-full w-40 mx-auto">
               Iniciar sesión
             </button>
           </form>
         )}
 
-        {/* REGISTER */}
+        {/* Cuadros de texto de Crear contraseña*/}
         {tab === "register" && (
           <form class="flex flex-col gap-4">
 
@@ -124,7 +126,7 @@ export default function Auth() {
                 class="w-full mt-1 p-3 rounded-full bg-[#4D55CC] text-white outline-none"
               />
             </div>
-
+          {/*Boton para para crear la cuenta*/}
             <button class="mt-4 bg-indigo-800 hover:bg-[#B5A8D5] text-white py-3 rounded-full w-40 mx-auto">
               Crear cuenta
             </button>
