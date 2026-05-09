142dd1888f0f6ff20b76d87d8a61b7f7208c6b43
anavillegas26 <anivillegas26@gmail.com>
2026-05-09 17:57:39 -0400

    eliminar mockup.png del avance anterior

--- Tele_Alae/fresh.config.ts
+++ Tele_Alae/fresh.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "$fresh/server.ts";
+// Importa la función para configurar el servidor Fresh
+// El "$fresh/" es un alias especial de Deno, no una carpeta local
 import tailwind from "$fresh/plugins/tailwind.ts";
+// Importa el plugin de Tailwind CSS para que los estilos funcionen
 
 export default defineConfig({
   plugins: [tailwind()],
 });
+// Le dice a Fresh: "usa Tailwind como plugin de estilos"
